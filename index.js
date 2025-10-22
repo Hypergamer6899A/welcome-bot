1e623c1a7f08d89c16605fc7366178480804cf0e
Hypergamer6899A <bj951630@gmail.com>
2025-10-22 09:20:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,24 @@ const client = new Client({
 });
 
 const joinMessages = [
-  //"Welcome {Username} to {ServerName}!",
+  "{Servername} has joined {Username}... Wait I got it backwards, dang it",
   "{Username} has arrived, let's see how long they last...",
-  "{Username} just made the member count {PlayerCount}.",
-  "Good luck {Username}, you'll need it.",
+  "{Username} just made the member count {PlayerCount}",
+  "Good luck {Username}, you'll need it",
   "{PlayerCount} members now counting {Username}!",
   "{Username}? That's an interesting name...",
-  //"{Username} has dropped by {ServerName}.",
+  "No way, could it be? Is it... oh wait, it's just {Username}",
   "{ServerName} has a brand new member. It's the one, the only, {Username}!",
   "{Username} just got kidnapped",
-  "{Username} discovered the nether"
+  "{Username} discovered the nether",
+  "{Username} might just be the reason I quit",
+  "{Username} is here, how many more people are gonna join?",
+  "{Username} joined {ServerName}, or did they?",
+  "Hey {Username}, {ServerName} here. Your home security system is great! Or is it?",
+  "Do we really need {Username}? Oh wait they're already here",
+  "It's a bird, it's a plane, it's... it's {Username}!",
+  "{ServerName} now has {Username} to worry about",
+  "Welcome {Username}. Yes I got lazy with this message, don't judge me"
 ];
 
 // --- Helper ---
